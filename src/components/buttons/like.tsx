2eb5b44388fecf9c3447bcffc0acc16e87282e33
Sayam Ghosh <sayamghosh.dev@gmail.com>
2025-04-14 14:28:51 +0530

    liked blog color red done

--- src/components/buttons/like.tsx
+++ src/components/buttons/like.tsx
@@ -1,51 +1,81 @@
-'use client'
-import React from 'react'
-import {Heart} from 'lucide-react'
-import { useState ,useEffect} from 'react';
-import {getAuthToken} from '@/utils/cookie-store'
+"use client";
+import React from "react";
+import { Heart } from "lucide-react";
+import { useState, useEffect } from "react";
+import { getAuthToken } from "@/utils/cookie-store";
 
-export default function Like({id}:{id:string}) {
-  
+export default function Like({ id }: { id: string }) {
   const [likes, setLikes] = useState(0);
-  // const [liked, setLiked] = useState(false);
+  const [isLiked, setIsLiked] = useState(false);
 
-  async function fetchLikes(){
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/like/count`, {
-      method: 'POST',
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ blogId: id }),
-    })
+  async function fetchLikes() {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/like/count`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify({ blogId: id }),
+      }
+    );
     const data = await res.json();
     setLikes(data.count);
-    console.log(data.count);
+  }
+
+  async function fetchIsLiked() {
+    const token = await getAuthToken();
+    if(!token) {
+      return;
+    }
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/like/check`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+         },
+        body: JSON.stringify({ blogId: id}),
+      }
+    );
+    const data = await res.json();
+    console.log(data.liked);
+    setIsLiked(data.liked);
   }
 
   useEffect(() => {
-    fetchLikes();
+    fetchIsLiked();
   }, [id]);
 
-  async function handleLike(){
-    const token = getAuthToken()
-    if(!token){
-      return  //TODO: ADD TOAST MESSAGE
+  useEffect(() => {
+    fetchLikes();
+  }, [isLiked]);
+
+  async function handleLike() {
+    const token = getAuthToken();
+    if (!token) {
+      return; //TODO: ADD TOAST MESSAGE
     }
     try {
-      await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/like/toggle`,{
-        method:"POST",
-        headers:{
+      await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/like/toggle`, {
+        method: "POST",
+        headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${await getAuthToken()}`
+          Authorization: `Bearer ${await getAuthToken()}`,
         },
         body: JSON.stringify({ blogId: id }),
-      })
-      fetchLikes()
+      });
+      fetchLikes();
+      fetchIsLiked();
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-    
   }
 
   return (
-    <div onClick={handleLike} className='w-fit px-3 py-1 flex gap-2 border rounded-full items-center cursor-pointer'><Heart size={15} /><p>{likes}</p></div>
-  )
+    <div
+      onClick={handleLike}
+      className="w-fit  px-3 py-1 flex gap-2 border rounded-full items-center cursor-pointer">
+      <Heart className={`${isLiked ? "text-red-500":"text-white"}`} size={15} />
+      <p className="text-white">{likes}</p>
+    </div>
+  );
 }
